b9d7843a4e839c444b1642e4b96288216b8f70bd
Yakari144 <yakari144@DESKTOP-MANKC29.localdomain>
2023-06-20 15:31:52 +0100

    a

---
+++ app/routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var data = new Date().toISOString().substring(0,16)
+  console.log("Na cb da página principal...")
+  console.log("Sessão: " + req.sessionID)
+  if(req.user)
+    res.render('index', {d: data, u: req.user});
+  else
+    res.render('index', {d: data});
+});
+
+function verificaAutenticacao(req, res, next){
+  console.log('User (verif.): ' + JSON.stringify(req.user))
+  if(req.isAuthenticated()){
+  //req.isAuthenticated() will return true if user is logged in
+      next();
+  } else{
+    res.redirect("/users/login");
+  }
+}
+
+router.get('/protegida', verificaAutenticacao, (req,res) => {
+  var data = new Date().toISOString().substring(0,16)
+  res.render('protected', {d: data, u: req.user})
+}
+)
+
+module.exports = router;
---
+++ app/routes/users.js
@@ -0,0 +1,35 @@
+﻿var express = require('express');
+var router = express.Router();
+var passport = require('passport')
+
+/* GET users listing. */
+router. get ('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router. get ('/login', function (req, res) {
+    var data = new Date().toISOString().substring(0, 16)
+    console.log('Na cb do GET login...')
+    console.log(req.sessionID)
+    res.render('login', {
+        d: data
+    })
+})
+
+router. get ('/logout', function (req, res) {
+  req.logout(function(err) {
+    if (err) 
+      res.render('error', {error: err})
+    else
+      res.redirect('/');
+  })
+})
+
+router.post('/login', passport.authenticate('local'),
+function (req, res) {
+    console.log('Na cb do POST login...')
+    console.log('Auth: ' + JSON.stringify(req.user))
+    res.redirect('/protegida')
+})
+
+module.exports = router;
